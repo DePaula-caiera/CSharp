2ad737fd50e58a3cc4b9ce72d463f39702ccf96b
Caiera03 <caiodepaula90@hotmail.com>
2023-09-28 18:47:49 -0300

    commit Aula 86.

---
+++ OO/Abstract.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.OO
+{
+    //Classe abstract pode ser herdada por outras classes, mas não pode ser instanciada.
+    //o que for recebiode herança e for abstrato, é necessário declarar/implementar. Ex: Assitente()
+    public abstract class Celular
+    {
+        public abstract string Assistente();
+
+        public string Tocar()
+        {
+            return "Trim. Trim. Triiim...";
+        }
+    }
+
+    public class Samsung : Celular
+    {
+        public override string Assistente() 
+        {
+            return "Olá. Meu nome é Bixby.";
+        }
+    }
+
+    public class Iphone : Celular
+    {
+        public override string Assistente()
+        {
+            return "Olá. Meu nome é Siri.";
+        }
+
+    }
+
+    class Abstract
+    {
+        public static void Executar()
+        {
+            var celulares = new List<Celular>
+            {
+                new Samsung(),
+                new Iphone()
+            };
+
+            foreach (var celular in celulares) 
+            {
+                Console.WriteLine(celular.Assistente());
+            }
+        }
+    }
+}
---
+++ OO/Sealed.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.OO
+{//classe com modificador sealed não permite herança e nem sobreescrita
+ // classe sealed não pode ser herdada, e método sealed não pode ser sobreescrito
+
+    sealed class SemFilho
+    {
+        public double ValorDaFortuna()
+        {
+            return 1_410_000.88;
+        }
+    }
+
+    //class SouFilho : SemFilho
+    //{
+
+    //}
+
+    class Avo
+    {// virtual permite sobreescrita
+        public virtual bool HonrarNomeFamilia()
+        {
+            return true;
+        }
+    }
+
+    class Pai : Avo
+    {
+        public override sealed bool HonrarNomeFamilia()
+        {
+            return true;
+        }
+    }
+
+    class FilhoRebelde : Pai
+    {
+        //public override bool HonrarNomeFamilia()
+        //{
+        //    return false;
+        //}
+    }
+
+    class Sealed
+    {
+        public static void Executar()
+        {
+            SemFilho semFilho = new SemFilho();
+            Console.WriteLine(semFilho.ValorDaFortuna());
+
+            FilhoRebelde filho = new FilhoRebelde();
+            Console.WriteLine(filho.HonrarNomeFamilia());
+        }
+    }
+}
